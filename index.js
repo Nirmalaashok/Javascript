369407efa64928f8518a959a2865b37e565ccdcf
Nirmalaashok <148657928+Nirmalaashok@users.noreply.github.com>
2023-11-28 22:34:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("Hello World!")
+console.log('Im awesome');
+// TODO -> here you initialize your variable
+console.log("the value of my variable x will be: 200");
+let G=5;
+console.log(G);
+G=20
+let z= 7.25;
+console.log("The round value of Z:"+Math.round(z)); 
+let animals=['elephant', 'Cat', 'Dog', 'Tiger', 'Lion'];
+console.log(animals[3]);
+     let stringNam = "Nirmala Ashok";
+    let lengthOfName =stringNam.length;
+    console.log("Length of the Name:" +lengthOfName)
+    let var1 = 20;
+let var2 = 100;
+function checkSameType(var1, var2) {
+  if (typeof var1 === typeof var2) {
+    console.log("SAME");
+  } else {
+    console.log("Different");
+  }
+}
+checkSameType(var1, var2);
+let x = 7;
+x = x % 3;
+
+console.log(x);
+let multipleArray = [42, "Hello", true, 3.14,];
+
+// Mutliple Arrays
+console.log("Array:", multipleArray);
+console.log("Element at index 0:", multipleArray[0]);  // Num
+console.log("Element at index 1:", multipleArray[1]);  // Str
+console.log("Element at index 2:", multipleArray[2]);  // Boolean
+console.log("Element at index 3:", multipleArray[3]);  // Num
+
+// Modifying 
+multipleArray[1] = "World";
+console.log("Modified array:",multipleArray[1]);
+
+
+
